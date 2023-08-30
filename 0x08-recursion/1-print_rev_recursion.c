838863dcae2fc05e009ae59ab41dd3adfa16b7a0
samz69 <esamalsunni69@gmail.com>
2023-08-30 10:21:28 -0700

    T1-2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints string in reverse
- * @s: pointer to string
- * *Return: void
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-	if (*s > '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 	}
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen_rescursion - returns the length of a string
- * @s: pointer the string
- * Return: int
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
  */
-
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
-	int ntr = 0;
+	int longit = 0;
 
-	if (*s > '\0')
+	if (*s)
 	{
-		ntr += _strlen_recursion(s + 1) + 1;
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	return (ntr);
+
+	return (longit);
 }
